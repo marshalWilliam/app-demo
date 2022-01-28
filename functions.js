ebd4f894c2deca4d7c3fbe1ee812d1607c250c79
marshalWilliam <98588086+marshalWilliam@users.noreply.github.com>
2022-01-28 18:44:24 +0800

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,24 @@
+const users = require('./data.js')
+const {writeFileSync} = require('fs')
+
+const addUser = (fname,lname) => {
+    const user = users.slice(-1)
+    const id = user[0].id + 1
+    users.push({id,fname,lname})
+    writeFileSync('./data.js',`const users = ${JSON.stringify(users,null,2)} \n module.exports = users`)
+}
+
+const updateUser = (fname,lname,id) => {
+    const index = users.findIndex( (user) => user.id === id)
+    users[index].fname = fname
+    users[index].lname = lname
+    writeFileSync('./data.js',`const users = ${JSON.stringify(users,null,2)} \n module.exports = users`)
+}
+
+const deleteUser = (id) => {
+    const index = users.findIndex( (user) => user.id === id)
+    users.splice(index,1)
+    writeFileSync('./data.js',`const users = ${JSON.stringify(users,null,2)} \n module.exports = users`)
+}
+
+module.exports = {addUser,updateUser,deleteUser}
